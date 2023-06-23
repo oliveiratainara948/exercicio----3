230b64fa37f51058ddf8478f2900860541fa4655
Taii <126676872+oliveiratainara948@users.noreply.github.com>
2023-06-23 16:56:52 -0300

    commit

---
+++ scriot.js
@@ -0,0 +1,8 @@
+let contador
+let numero = prompt('Escreva um número inteiro e positivo')
+
+while (contador < numero) {
+    let resultado = contador;
+    console.log(contador);
+    contador++;
+}
